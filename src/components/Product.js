0163cce975a9cc628368387d3f10e4aff0acd958
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-26 20:28:03 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,17 +9,19 @@ class Order extends React.Component {
   }
 }
 
-Order.defaultProps = {
-  cone: true,
-  size: 'regular'
+Product.defaultProps = {
+  hasWatermark: false
 };
 
-Order.propTypes = {
-  cone: PropTypes.bool,
-  size: PropTypes.string,
-  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool //arrayOf(PropTypes.string).isRequired,
+  color: PropTypes.string.isRequired, // a string — required, can only be `'white'`, `'eggshell-white'` or `'salmon'`
+  weight: PropTypes.int.isRequired // a number — required, ranges between 80 and 300
   orderInfo: PropTypes.shape({
     customerName: PropTypes.string.isRequired,
     orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
   }).isRequired
 };
+
